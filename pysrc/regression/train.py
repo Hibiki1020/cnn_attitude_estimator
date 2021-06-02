f086b6ad8a21c13acbad444bcb5419e62b31c267
Hibiki Kawai <hibikijitaku@gmail.com>
2021-06-02 16:55:44 +0900

    学習用プログラム完成(動作未確認)

--- pysrc/regression/train.py
+++ pysrc/regression/train.py
@@ -48,3 +48,80 @@ if __name__ == '__main__':
         print(e)
         print("Error opening train config file %s", FLAGS.train_cfg)
         quit()
+
+    method_name = CFG["method_name"]
+    save_top_path = CFG["save_top_path"]
+    weights_path = CFG["save_top_path"] + CFG["weights_path"]
+    log_path = CFG["save_top_path"] + CFG["log_path"]
+    graph_path = CFG["save_top_path"] + CFG["graph_path"]
+    csv_name = CFG["csv_name"]
+
+    train_sequences = CFG["train"]
+    valid_sequences = CFG["valid"]
+
+    #get hyperparameter for learning
+    resize = CFG["hyperparameter"]["resize"]
+    mean_element = CFG["hyperparameter"]["mean_element"]
+    std_element = CFG["hyperparameter"]["std_element"]
+    hor_fov_deg = CFG["hyperparameter"]["hor_fov_deg"]
+    optimizer_name = CFG["hyperparameter"]["optimizer_name"]
+    lr_cnn = float(CFG["hyperparameter"]["lr_cnn"])
+    lr_fc = float(CFG["hyperparameter"]["lr_fc"])
+    batch_size = CFG["hyperparameter"]["batch_size"]
+    num_epochs = CFG["hyperparameter"]["num_epochs"]
+
+    try:
+        print("Copy files to %s for further reference." % log_path)
+        copyfile(FLAGS.train_cfg, save_top_path + log_path + "/train_config.yaml")
+    except Exception as e:
+        print(e)
+        print("Error copying files, check permissions. Exiting....")
+        quit()
+
+    train_dataset = dataset_mod.Originaldataset(
+        data_list = make_datalist_mod.makeMultiDataList(train_sequences, csv_name),
+        transform = data_transform_mod.DataTransform(
+            resize,
+            ([mean_element, mean_element, mean_element]),
+            ([std_element, std_element, std_element]),
+            hor_fov_deg = hor_fov_deg
+        ),
+        phase = "train"
+    )
+
+    valid_dataset = dataset_mod.Originaldataset(
+        data_list = make_datalist_mod.makeMultiDataList(valid_sequences, csv_name),
+        transform = data_transform_mod.DataTransform(
+            resize,
+            ([mean_element, mean_element, mean_element]),
+            ([std_element, std_element, std_element]),
+            hor_fov_deg = hor_fov_deg
+        ),
+        phase = "valid"
+    )
+
+    ##Network
+    net = network_mod.Network(resize, dim_fc_out=3, dropout_rate=0.1, use_pretrained_vgg=True)
+
+
+    ##Criterion
+    criterion = nn.MSELoss()
+
+    #train
+    trainer = trainer_mod.Trainer(
+        method_name,
+        train_dataset,
+        valid_dataset,
+        net,
+        criterion,
+        optimizer_name,
+        lr_cnn,
+        lr_fc,
+        batch_size,
+        num_epochs,
+        weights_path,
+        log_path,
+        graph_path
+    )
+
+    trainer.train()
