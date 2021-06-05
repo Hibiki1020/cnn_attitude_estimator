03f0ac3fa7b6437ad985558083733050a5955147
Hibiki Kawai <hibikijitaku@gmail.com>
2021-06-05 14:20:54 +0900

    change donfig data

--- pysrc/common/network_mod.py
+++ pysrc/common/network_mod.py
@@ -16,9 +16,9 @@ class Network(nn.Module):
         self.conv2 = nn.Conv2d(  64, 128, 4)
         self.conv3 = nn.Conv2d( 128, 256, 3)
         self.conv4 = nn.Conv2d( 256, 512, 3)
-        self.conv5 = nn.Conv2d( 512, 1024, 3)
+        self.conv5 = nn.Conv2d( 512, 512, 3)
 
-        dim_fc_in = 1024*(resize//32)*(resize//32)
+        dim_fc_in = 512*(resize//32)*(resize//32)
 
         self.fc1 = nn.Linear(dim_fc_in, dim_fc_out)
         self.fc2 = nn.Linear( dim_fc_out, dim_fc_out)
