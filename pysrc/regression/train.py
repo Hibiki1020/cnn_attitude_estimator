9f5921d76a1f00a9dbc48ff04d56a8bebee4f881
Hibiki Kawai <hibikijitaku@gmail.com>
2021-06-05 13:29:54 +0900

    change donfig data

--- pysrc/regression/train.py
+++ pysrc/regression/train.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
 
     try:
         print("Copy files to %s for further reference." % log_path)
-        copyfile(FLAGS.train_cfg, save_top_path + log_path + "/train_config.yaml")
+        copyfile(FLAGS.train_cfg, log_path + "/train_config.yaml")
     except Exception as e:
         print(e)
         print("Error copying files, check permissions. Exiting....")
