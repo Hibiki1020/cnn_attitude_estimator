afaff390c15d058d4275a524cba7274b5521402b
Hibiki Kawai <hibikijitaku@gmail.com>
2021-06-05 16:09:11 +0900

    change donfig data

--- pysrc/common/network_mod.py
+++ pysrc/common/network_mod.py
@@ -18,7 +18,7 @@ class Network(nn.Module):
         self.conv4 = nn.Conv2d( 256, 512, 3)
         self.conv5 = nn.Conv2d( 512, 1024, 3)
 
-        self.dim_fc_in = 1024*19*19*10
+        self.dim_fc_in = 1024*19*19*0.1
         self.dim_fc_out = dim_fc_out
 
         #test code
