53473a00f3884d3cce8c5b905c20683fcc01ea3a
Hibiki Kawai <hibikijitaku@gmail.com>
2021-06-05 15:16:31 +0900

    change donfig data

--- pysrc/common/dataset_mod.py
+++ pysrc/common/dataset_mod.py
@@ -108,16 +108,16 @@ class ClassOriginaldataset(data.Dataset):
             array[i] = 0.0
         
         tmp_index = self.search_index( str(int(r_ud[0])), str(int(r_ud[1])) )
-        array[tmp_index] = values[0]
+        array[tmp_index] = str(values[0])
 
         tmp_index = self.search_index( str(int(r_up[0])), str(int(r_up[1])) )
-        array[tmp_index] = values[1]
+        array[tmp_index] = str(values[1])
 
         tmp_index = self.search_index( str(int(l_ud[0])), str(int(l_ud[1])) )
-        array[tmp_index] = values[2]
+        array[tmp_index] = str(values[2])
 
         tmp_index = self.search_index( str(int(l_up[0])), str(int(l_up[1])) )
-        array[tmp_index] = values[3]
+        array[tmp_index] = str(values[3])
 
         return array
 
