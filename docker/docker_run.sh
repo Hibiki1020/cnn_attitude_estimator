7f9050b1455a59a75cd2926b21cba7e663b56c7e
Hibiki Kawai <hibikijitaku@gmail.com>
2021-06-12 11:43:36 +0900

    docker_run.sh changed

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -15,5 +15,5 @@ docker run -it --rm \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --net=host \
-    -v /media/amsl/96fde31e-3b9b-4160-8d8a-a4b913579ca21:/home/ssd_dir \
+    -v /home/kawai/airsim_dataset:/home/ssd_dir \
 	$image_name:$tag_name
