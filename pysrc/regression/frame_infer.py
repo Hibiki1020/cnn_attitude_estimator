b7f77db12ae19f5d12d70ec9b62a27eefeca6409
Hibiki Kawai <hibikijitaku@gmail.com>
2021-07-06 13:25:24 +0900

    alpha and clip_limit changed

--- pysrc/regression/frame_infer.py
+++ pysrc/regression/frame_infer.py
@@ -208,8 +208,8 @@ class CNNAttitudeEstimator:
 
         print(logged_output_roll_array)
 
-        #output_roll_array = torch.pow(10.0, logged_output_roll_array)
-        #output_pitch_array = torch.pow(10.0, logged_output_pitch_array)
+        output_roll_array = torch.pow(10.0, logged_output_roll_array)
+        output_pitch_array = torch.pow(10.0, logged_output_pitch_array)
 
         #softmaxにしても大小関係は変わらない
         #output_roll_array = nn_functional.softmax(logged_output_roll_array)
@@ -217,8 +217,8 @@ class CNNAttitudeEstimator:
 
         #print(output_roll_array)
         
-        output_roll_array = torch.exp(logged_output_roll_array)
-        output_pitch_array = torch.exp(logged_output_pitch_array)
+        #output_roll_array = torch.exp(logged_output_roll_array)
+        #output_pitch_array = torch.exp(logged_output_pitch_array)
 
         print(output_pitch_array)
 
