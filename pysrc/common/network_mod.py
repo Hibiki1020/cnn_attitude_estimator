3a0e806282e4c5788072028ed15d9c885ca75c5b
Hibiki Kawai <hibikijitaku@gmail.com>
2021-07-11 14:53:22 +0900

    change alpha

--- pysrc/common/network_mod.py
+++ pysrc/common/network_mod.py
@@ -96,8 +96,10 @@ class Network(nn.Module):
         logged_pitch = nn_functional.log_softmax(pitch, dim=1)
 
         torch.set_printoptions(edgeitems=10000)
-        #print(feature)
-        print(self.roll_fc[6].weight.data)
+        print(feature)
+
+        #Weights no problem
+        #print(self.roll_fc[6].weight.data)
         print(roll)
         print(logged_roll)
 
