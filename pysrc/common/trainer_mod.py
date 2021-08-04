c8cc781a9cb4a9039722242994346240ca4fe1de
Hibiki Kawai <hibikijitaku@gmail.com>
2021-08-04 16:17:43 +0900

    Infer Test

--- pysrc/common/trainer_mod.py
+++ pysrc/common/trainer_mod.py
@@ -218,7 +218,9 @@ class Trainer:
                         #print(tmpshow_array)
                     
                         torch.set_printoptions(edgeitems=10000)
-                        #print(label_pitch)
+                        print(roll_inf)
+                        print(label_pitch)
+                        print(roll_loss)
 
                         #roll_loss = torch.mean( -label_roll * logged_roll_inf )
                         #pitch_loss = torch.mean( -label_pitch * logged_pitch_inf )
