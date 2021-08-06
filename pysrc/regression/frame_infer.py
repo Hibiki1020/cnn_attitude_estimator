cdc5090a4da648416aad29cea84c30f8c364300e
Hibiki Kawai <hibikijitaku@gmail.com>
2021-08-06 20:18:19 +0900

    Infer Test

--- pysrc/regression/frame_infer.py
+++ pysrc/regression/frame_infer.py
@@ -423,9 +423,9 @@ class CNNAttitudeEstimator:
             plt.rcParams['agg.path.chunksize'] = 10000
 
             plt.grid()
-            #plt.show()
+            plt.show()
 
-            #self.show_fig(roll_hist_array, pitch_hist_array, self.value_dict, windows[1])
+            self.show_fig(roll_hist_array, pitch_hist_array, self.value_dict, windows[1])
 
             cov = np.cov(np_result)
             
