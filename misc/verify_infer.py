b3ef0f1a81d9284e236d5a237ad767606d285ff3
Hibiki Kawai <hibikijitaku@gmail.com>
2021-08-09 13:56:11 +0900

    add variance

--- misc/verify_infer.py
+++ misc/verify_infer.py
@@ -2,6 +2,8 @@ import yaml
 import csv
 import argparse
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 class VerifyInfer:
     def __init__(self, CFG):
@@ -9,18 +11,22 @@ class VerifyInfer:
         self.infer_log_top_path = CFG["infer_log_top_path"]
         self.infer_log_file_name = CFG["infer_log_file_name"]
 
-        self.data_list = self.data_load()
+        self.data_list, self.roll_variance, self.pitch_variance = self.data_load()
 
     def data_load(self):
         csv_path = os.path.join(self.infer_log_top_path, self.infer_log_file_name)
         data_list = []
+        roll_val_list = []
+        pitch_val_list = []
         
         with open(csv_path) as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
                 data_list.append(row)
+                roll_val_list.append(row[5])
+                pitch_val_list.append(row[6])
 
-        return data_list
+        return data_list, roll_val_list, pitch_val_list
 
     def spin(self):
         acc_roll_diff = 0.0
@@ -29,9 +35,15 @@ class VerifyInfer:
         acc_pitch_variance = 0.0
         size_of_csv = float(len(self.data_list))
 
+        roll_diff_list = []
+        pitch_diff_list = []
+
         for row in self.data_list:
             tmp_roll_diff = abs(float(row[1]) - float(row[3]))
             tmp_pitch_diff = abs(float(row[2]) - float(row[4]))
+
+            roll_diff_list.append(tmp_roll_diff)
+            pitch_diff_list.append(tmp_pitch_diff)
 
             acc_roll_diff += tmp_roll_diff
             acc_pitch_diff += tmp_pitch_diff
@@ -47,6 +59,9 @@ class VerifyInfer:
         print("Average Pitch Difference :" + str(acc_pitch_diff) + " [deg]")
         print("Average Roll Variance  :" + str(acc_roll_variance))
         print("Average Pitch Variance :" + str(acc_pitch_variance))
+
+        plt.scatter(self.roll_variance, roll_diff_list)
+        plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("./verify_infer.py")
