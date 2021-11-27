5cb8b7d38d8faa64cdde4d6e376b7e80ebd2f407
Hibiki1020 <hibikijitaku@gmail.com>
2021-11-27 20:06:22 +0900

    Catch IMU's orientation data and convert euler angle and save as csv file

--- pysrc/misc/convert_quaternion_to_rpy.py
+++ pysrc/misc/convert_quaternion_to_rpy.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import rospy
 from sensor_msgs.msg import Imu
 
@@ -16,26 +18,30 @@ class QuaternionToRpy:
     def __init__(self):
         self.imu_data = Imu()
         self.imu_subscriber = rospy.Subscriber('/imu/data', Imu, self.callbackImuMsg, queue_size=1)
-        self.init_csv('/media/amsl/96fde31e-3b9b-4160-8d8a-a4b913579ca21/tmp_save/imu_attitude', '/imu_attitude.csv')
+        self.init_csv('/media/amsl/96fde31e-3b9b-4160-8d8a-a4b913579ca21/tmp_save/imu_attitude', 'imu_attitude.csv')
         self.csv_path = 'init_path'
         self.count = 0
         self.init_sec = 0
     
     def init_csv(self, top_path, filename):
         self.csv_path = os.path.join(top_path, filename)
-        self.csv_file = open(self.csv_path, 'w')
+        self.csv_file = open(self.csv_path, 'a')
         self.csv_w = csv.writer(self.csv_file)
 
     def callbackImuMsg(self, msg):
         print("Catch IMU Data")
         self.imu_data = msg
-        eular = tf.transformations.eular_from_quaternion(self.imu_data.orientation.x, self.imu_data.orientation.y, self.imu_data.orientation.z, self.imu_data.orientation.w)
-        time = rospy.to_sec(self.imu_data.header.stamp)
+        quaternion = (self.imu_data.orientation.x, self.imu_data.orientation.y, self.imu_data.orientation.z, self.imu_data.orientation.w)
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+        
+        time = self.imu_data.header.stamp.to_sec()
         
         if self.count == 0:
             self.init_sec = time
         
-        array = [str(time), str(eular[0]), str(eular[1]), str(eular[2])]
+        array = [str(time-self.init_sec), str(euler[0]), str(euler[1]), str(euler[2])]
+        print(array)
+        self.csv_w.writerow(array)
         self.count += 1
 
 
